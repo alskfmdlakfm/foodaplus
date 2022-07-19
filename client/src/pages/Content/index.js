1a050734531e0644fe9d3f98c57f61936f41634f
Sooyeon Oh <sooyeon.oh@mongodb.com>
2022-07-19 16:48:21 -0400

    Add static stars to vendors page

--- client/src/pages/Content/index.js
+++ client/src/pages/Content/index.js
@@ -1,14 +1,19 @@
 console.log('Content script works!');
 console.log('Must reload extension for modifications to take effect.');
 
+const starImageSrc = "https://img.icons8.com/fluency/48/000000/star.png";
+
 onLoad();
 
 function onLoad() {
   const vendorTilesInfo = document.getElementsByClassName("myfooda-event__meta")
   for (let i = 0; i < vendorTilesInfo.length; i++) {
+    // add logic to retrieve number of stars from server and calculate stars to use
     const stars = document.createElement("div");
+    stars.className = "starsContainer";
     const star = document.createElement("img");
-    //star.setAttribute("src", "/assets/img/icons8-star-48.png");
+    star.src = starImageSrc;
+    star.className = "star"
     stars.appendChild(star);
     vendorTilesInfo[i].appendChild(stars);
   }
