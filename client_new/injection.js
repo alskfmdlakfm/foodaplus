75e106d1e8c86b4490841e760ee76f5f43fc40cf
Zackary Santana <zackzackyack@gmail.com>
2022-07-20 14:14:09 -0400

    Migrated files as well

--- client_new/injection.js
+++ client_new/injection.js
@@ -1,6 +1,24 @@
-const mainFunction = () => {
-  // IDK do code here
-  console.log("IT WORKS")
-};
-mainFunction();
+import fullStarSrc from '../../assets/img/full-star-48.png';
+import halfStarSrc from '../../assets/img/half-star-48.png';
 
+onLoad();
+
+function onLoad() {
+  const vendorTilesInfo = document.getElementsByClassName("myfooda-event__meta")
+  for (let i = 0; i < vendorTilesInfo.length; i++) {
+    const stars = document.createElement("div");
+    stars.className = "starsContainer";
+    // add logic to retrieve number of stars from server and calculate stars to use
+    for (let i = 0; i < 3; i++) {
+        const star = document.createElement("img");
+        star.className = "star"
+        star.src = chrome.runtime.getURL('assets/img/full-star-48.png');
+        stars.appendChild(star);
+    }
+    const halfStar = document.createElement("img");
+    halfStar.className = "star"
+    halfStar.src = chrome.runtime.getURL('assets/img/half-star-48.png');
+    stars.appendChild(halfStar);
+    vendorTilesInfo[i].appendChild(stars);
+  }
+}
