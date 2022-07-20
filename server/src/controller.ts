f210886b9e902f1b349c54151e988a1097f58b59
Zackary Santana <zackzackyack@gmail.com>
2022-07-20 12:26:03 -0400

    Added some write review logic and fixed schema

--- server/src/controller.ts
+++ server/src/controller.ts
@@ -2,7 +2,6 @@ import { Prisma, PrismaClient, Review, Vendor } from '@prisma/client'
 
 const prisma = new PrismaClient();
 
-
 export async function getVendor(name: string) {
     const result = await prisma.vendor.findFirstOrThrow({
         where: { name }
@@ -19,18 +18,36 @@ export async function pushVendor(newVendor: any){
     return !!vendor;
 }
 
-export async function writeReview(newReview: Review, vendorName: string) {
-    const review = await prisma.review.create({ data: newReview });
-    if (review){
+export async function writeReview (newReview: Review, vendorName: string) {
+    // const review = await prisma.review.create({ data: newReview });
+    const vendor = await prisma.vendor.findUnique({where: {name: vendorName}, include: {reviews: true}});
+    if (!vendor){
         // TODO: make this work
-        // await prisma.vendor.update({
-        //     where: { name: vendorName },
-        //     data: {
-        //         reviews: {
-        //             push: review
-        //         }
-        //     }
-        // })
+        return await prisma.vendor.create({
+            data: {
+                name: vendorName,
+                rating: newReview.rating,
+                numReviews: 1,
+                reviews: {
+                    create: [newReview]
+                }
+            }
+        })
+    } else if (vendor != null) {
+        const currentRating = (vendor.rating ?? 0);
+        const reviewAmount = (vendor.numReviews ?? 0);
+        const newRating = (currentRating / (reviewAmount == 0 ? 1 : reviewAmount) 
+            + newReview.rating) / (reviewAmount + 1);
+            const reviews = [...vendor.reviews, newReview] as Review[];
+        return await prisma.vendor.update({
+            where: {name:vendorName},
+            data: {
+                rating: newRating,
+                badges: "lol todo",
+                numReviews: reviewAmount + 1,
+                // reviews: newReview,
+            }
+        })
     }
 }
 
