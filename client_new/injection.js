aea0fe2d057a3059cf8568017ccad305705194fe
Zackary Santana <zackzackyack@gmail.com>
2022-07-21 09:52:46 -0400

    Put bootstrap callback in a promise to do it async

--- client_new/injection.js
+++ client_new/injection.js
@@ -138,11 +138,14 @@ const createBadge = (parent, title, count) => {
 }
 
 const loadBootstrap = () => {
-  const bootstrap = document.createElement("link");
-  bootstrap.href = "https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css";
-  bootstrap.rel = "stylesheet";
-  bootstrap.crossorigin = "anonymous";
-  document.head.appendChild(bootstrap);
+  new Promise((resolve => {
+    const bootstrap = document.createElement("link");
+    bootstrap.href = "https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css";
+    bootstrap.rel = "stylesheet";
+    bootstrap.crossorigin = "anonymous";
+    document.head.appendChild(bootstrap);
+    resolve();
+  }))
 }
 
 const goodBadges = new Set(["Arrives on time", "Good value", "Tastes good"]);
